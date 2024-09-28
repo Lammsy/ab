1fb5eb31034781fd887a754a27e940ab32ec0a4f
Lammsy <zxzxzxzx4114@gmail.com>
2024-09-27 19:02:42 -0700

    asd

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -137,52 +137,83 @@ exports.handler = async function(event, context) {
     try {
         const { i } = event.queryStringParameters;
         const { t } = event.queryStringParameters;
-        const { data } = await axios.get('https://pornhub.com/video?o=mv&page=' + rand(0, 455), {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
-            }
-        });
+        if(i){
 
-        const $ = cheerio.load(data);
-        const videoElements = $('.pcVideoListItem.js-pop.videoblock.videoBox');
-        const links = [];
-        const titles=[];
-        const images =[];
-        const thvids = [];
-        videoElements.each((index, element) => {
-        const anchor = $(element).find('a').first(); // Get the first <a> element
-        const imgElement =$(element).find('img').first();
-        const img = imgElement.attr('src');
-        const href = anchor.attr('href'); // Extract the href attribute
-        const tit = anchor.attr('title'); // Extract the href attribute
-        const thvid = imgElement.attr('data-mediabook'); // Extract the href attribute
-        if (href) {
-            links.push(href); // Add the href to the links1 array
-        }
-        if(tit){
-            titles.push(tit);
-        }
-        if(img){
-            images.push(img);
-        }
-        if(thvid){
-            thvids.push(thvid);
-        }
-        });
         
+        // Fetch the HTML content of the page that contains the iframe
+        const response = await axios.get('https://www.pornhub.com/embed/'+i);
+        const $ = cheerio.load(response.data);
+
+        // Assuming you know the iframe selector
+        const iframeSrc = $('iframe').attr('src'); // Get the src attribute of the iframe
+        if (!iframeSrc) {
+            return res.status(404).json({ error: 'Iframe not found' });
+        }
+
+        // Fetch the HTML content of the iframe
+        const iframeResponse = await axios.get(iframeSrc);
+        const iframeHtml = iframeResponse.data;
+
         return {
             statusCode: 200,
             headers: {
                 "Access-Control-Allow-Origin": "*", // Allows any domain
             },
-            body: JSON.stringify({ links , titles , images,thvids}),
+            body: JSON.stringify({ iframeHtml}),
         };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: `Failed to scrape data: ${error.message}` }),
-        };
-    }
+
+
+        } else{
+            
+            const { data } = await axios.get('https://pornhub.com/video?o=mv&page=' + rand(0, 455), {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+                }
+            });
+    
+            const $ = cheerio.load(data);
+            const videoElements = $('.pcVideoListItem.js-pop.videoblock.videoBox');
+            const links = [];
+            const titles=[];
+            const images =[];
+            const thvids = [];
+            videoElements.each((index, element) => {
+            const anchor = $(element).find('a').first(); // Get the first <a> element
+            const imgElement =$(element).find('img').first();
+            const img = imgElement.attr('src');
+            const href = anchor.attr('href'); // Extract the href attribute
+            const tit = anchor.attr('title'); // Extract the href attribute
+            const thvid = imgElement.attr('data-mediabook'); // Extract the href attribute
+            if (href) {
+                links.push(href); // Add the href to the links1 array
+            }
+            if(tit){
+                titles.push(tit);
+            }
+            if(img){
+                images.push(img);
+            }
+            if(thvid){
+                thvids.push(thvid);
+            }
+            });
+            
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*", // Allows any domain
+                },
+                body: JSON.stringify({ links , titles , images,thvids}),
+            };
+        }
+    
+        }
+        catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ error: `Failed to scrape data: ${error.message}` }),
+            };
+        }
 };
 
 function rand(min, max) {
