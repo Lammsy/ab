278171d9e469ebb3bebc373e181c3c79d72df1d6
Lammsy <zxzxzxzx4114@gmail.com>
2024-10-13 20:50:17 -0700

    asd

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -217,10 +217,10 @@ exports.handler = async function(event, context) {
         const qualities = scriptContent.split('"quality":"');
 
         // Split script content to extract video URLs and get the first one
-        const vidlinks = scriptContent.split('"videoUrl":"https://cv-h.phncdn.com');
+        const vidlinks = scriptContent.split('"videoUrl\":\"https:\\/\\/cv-h.phncdn.com\\');
         
         // Ensure vidlinks array is not empty before accessing first element
-        const firstVidlink = vidlinks.length > 1 ? `https://cv-h.phncdn.com${vidlinks[1].split('"')[1]}` : null;
+        const firstVidlink = vidlinks.length > 1 ? `"videoUrl\":\"https:\\/\\/cv-h.phncdn.com\\${vidlinks[1].split('"')[1]}` : null;
         const tabline="--------------";
         //qualities.splice(index, 1);//remove index, number of el from index
         // Return the consolidated results
